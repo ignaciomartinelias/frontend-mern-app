f2f5b073e4c187aba750e907d6125a03c583f439
Ignacio Martin Elias <ignaciomartinelias@gmail.com>
2019-08-02 03:03:33 -0300

    First Commit React App

---
+++ precache-manifest.675518c0d5eb4766084d1a662618c8f8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c503e5efb46074b7ff6e263ac020221",
+    "url": "/frontend-mern-app/index.html"
+  },
+  {
+    "revision": "f871488569c181ec9e0d",
+    "url": "/frontend-mern-app/static/css/2.a30cb989.chunk.css"
+  },
+  {
+    "revision": "c90ce65413bd3de8f183",
+    "url": "/frontend-mern-app/static/css/main.bd4402f7.chunk.css"
+  },
+  {
+    "revision": "f871488569c181ec9e0d",
+    "url": "/frontend-mern-app/static/js/2.ec9a9eee.chunk.js"
+  },
+  {
+    "revision": "c90ce65413bd3de8f183",
+    "url": "/frontend-mern-app/static/js/main.22c3b832.chunk.js"
+  },
+  {
+    "revision": "01b3b98a449977025fcc",
+    "url": "/frontend-mern-app/static/js/runtime~main.c4480d47.js"
+  },
+  {
+    "revision": "060ea8dd8446c15159ed6ec217ae29b9",
+    "url": "/frontend-mern-app/static/media/fa-brands-400.060ea8dd.svg"
+  },
+  {
+    "revision": "4f786efdf6328877ec2448bf265bcc8f",
+    "url": "/frontend-mern-app/static/media/fa-brands-400.4f786efd.woff2"
+  },
+  {
+    "revision": "72f5c754189a296467db59e26e2b4671",
+    "url": "/frontend-mern-app/static/media/fa-brands-400.72f5c754.woff"
+  },
+  {
+    "revision": "75dae2b62813bc641a9b6a2fc041e4de",
+    "url": "/frontend-mern-app/static/media/fa-brands-400.75dae2b6.ttf"
+  },
+  {
+    "revision": "e33a1a54eb0a1e560362fcf5335ac26c",
+    "url": "/frontend-mern-app/static/media/fa-brands-400.e33a1a54.eot"
+  },
+  {
+    "revision": "0234a585809be071bd68db999f27f829",
+    "url": "/frontend-mern-app/static/media/fa-regular-400.0234a585.svg"
+  },
+  {
+    "revision": "76d069c84f54a63d1bf9e29c76fe65e6",
+    "url": "/frontend-mern-app/static/media/fa-regular-400.76d069c8.ttf"
+  },
+  {
+    "revision": "d90e1b03a9168c0d775a52098423adfb",
+    "url": "/frontend-mern-app/static/media/fa-regular-400.d90e1b03.woff2"
+  },
+  {
+    "revision": "d98ffd73c6b104b42bfe69c987a72924",
+    "url": "/frontend-mern-app/static/media/fa-regular-400.d98ffd73.eot"
+  },
+  {
+    "revision": "f9695ea8029b2975dea6753bf6f1bf1c",
+    "url": "/frontend-mern-app/static/media/fa-regular-400.f9695ea8.woff"
+  },
+  {
+    "revision": "55ce8d0ca92adb7c283d6043d0edf46f",
+    "url": "/frontend-mern-app/static/media/fa-solid-900.55ce8d0c.woff"
+  },
+  {
+    "revision": "60ce8cf4dd9fe177abdfeda21e20798e",
+    "url": "/frontend-mern-app/static/media/fa-solid-900.60ce8cf4.woff2"
+  },
+  {
+    "revision": "d42cc8bb088b9bbcb51b1db2d9210976",
+    "url": "/frontend-mern-app/static/media/fa-solid-900.d42cc8bb.svg"
+  },
+  {
+    "revision": "e9809ab6a5a34dc6b7966de82265d6bd",
+    "url": "/frontend-mern-app/static/media/fa-solid-900.e9809ab6.ttf"
+  },
+  {
+    "revision": "f9b8a1aa417e318d5be775e14ee3c475",
+    "url": "/frontend-mern-app/static/media/fa-solid-900.f9b8a1aa.eot"
+  }
+]);
